f945a6d855bf4edc450a5bc5024377a222e8d33c
mdariyanahmed2026 <mdshakib200814@gmail.com>
2025-12-17 10:32:30 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -1,57 +1,59 @@
 const fs = require("fs");
 
 module.exports = {
-  config: {
-    name: "help",
-    version: "2.0",
-    author: "Ariyan",
-    role: 0,
-    category: "info",
-    shortDescription: { en: "Show all commands" }
-  },
-
-  onStart: async function ({ api, event, role }) {
+  name: "help",
+  execute(api, event) {
     const commands = global.GoatBot.commands;
-    const prefix = "-";
+    const totalCmd = commands.size;
 
-    const categories = {};
-    let totalCmd = 0;
-
-    for (const [name, cmd] of commands) {
-      if (cmd.config?.role > role) continue;
-
-      const category = (cmd.config?.category || "OTHER").toUpperCase();
-      if (!categories[category]) categories[category] = [];
-      categories[category].push(name);
-      totalCmd++;
-    }
-
-    let msg = `
+    const menu = `
 ╔═══════════════╗
  🎏 𝗔𝗥𝗜𝗬𝗔𝗡 𝗕𝗢𝗧 𝙼𝙴𝙽𝚄
 ╚═══════════════╝
-`;
 
-    for (const cat in categories) {
-      msg += `\n┍━━━[ ${cat} ]☃\n`;
-      categories[cat].sort().forEach(cmd => {
-        msg += `┋ᐉ ${cmd}\n`;
-      });
-      msg += `┕━━━━━━━━━━━━━━◊\n`;
-    }
+┍━━━[ INFO ]☃
+┋ᐉ help
+┋ᐉ owner
+┋ᐉ uid
+┋ᐉ tid
+┕━━━━━━━━━━━━━━◊
 
-    msg += `
+┍━━━[ AI ]☃
+┋ᐉ ai
+┋ᐉ gpt
+┋ᐉ gemini
+┋ᐉ prompt
+┕━━━━━━━━━━━━━━◊
+
+┍━━━[ FUN ]☃
+┋ᐉ meme
+┋ᐉ joke
+┋ᐉ slap
+┋ᐉ kiss
+┕━━━━━━━━━━━━━━◊
+
+┍━━━[ GAME ]☃
+┋ᐉ quiz
+┋ᐉ guess
+┋ᐉ cricketgame
+┕━━━━━━━━━━━━━━◊
+
+┍━━━[ ISLAMIC ]☃
+┋ᐉ hadis
+┋ᐉ namaz
+┕━━━━━━━━━━━━━━◊
+
 ┍━━━━[𝗜𝗡𝗙𝗢𝗥𝗠]━━━━◊
 ┋➥ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗠𝗗: ${totalCmd}
-┋➥ 𝗣𝗥𝗘𝗙𝗜𝗫: ⦃ ${prefix} ⦄
-┋➥ 𝗢𝗪𝗡𝗘𝗥: ARIYAN
-┋➥ 𝗙𝗕: facebook.com/share/1A1Rurf6VA/
+┋➥ 𝗣𝗥𝗘𝗙𝗜𝗫:  ⦃ - ⦄
+┋➥ 𝗢𝗪𝗡𝗘𝗥:  ARIYAN
+┋➥ 𝗙𝗕: facebook.com/https://www.facebook.com/share/1D8QWsEgPS/
 ┕━━━━━━━━━━━━━━━◊
 `;
 
     api.sendMessage(
       {
-        body: msg,
+        body: menu,
         attachment: fs.createReadStream(__dirname + "/../ariyan.jpg")
       },
       event.threadID
